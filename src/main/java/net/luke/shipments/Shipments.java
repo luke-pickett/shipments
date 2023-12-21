7a3914a45323c9dd1d43c0d979665f6ed0ec9e9c
luke-pickett <engooeer@gmail.com>
2023-12-21 12:39:20 -0800

    fixed String name

--- src/main/java/net/luke/shipments/Shipments.java
+++ src/main/java/net/luke/shipments/Shipments.java
@@ -6,8 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Shipments implements ModInitializer {
-	public static final String modID = "shipments";
-    public static final Logger LOGGER = LoggerFactory.getLogger(modID);
+	public static final String modId = "shipments";
+    public static final Logger LOGGER = LoggerFactory.getLogger(modId);
 
 	@Override
 	public void onInitialize() {
